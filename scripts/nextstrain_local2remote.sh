61af328ee19d587022bceef917336b300dd68d2d
Katherine Eaton <ktmeaton@gmail.com>
2020-05-01 12:56:13 -0400

    accept cmd line args

--- scripts/nextstrain_local2remote.sh
+++ scripts/nextstrain_local2remote.sh
@@ -1,32 +1,20 @@
 #/bin/bash
 
-#MARKDOWN_LOCAL="narratives/plagueSCDS2020Local.md";
-#MARKDOWN_REMOTE="narratives/plagueSCDS2020Remote.md";
-#DATA_LOCAL="http://localhost:4000/plague150Local";
-#DATA_REMOTE="https://nextstrain.org/community/ktmeaton/plague-phylogeography/plague150Remote";
-#NARRATIVE_LOCAL="http://localhost:4000/narratives/plagueSCDS2020Local";
-#NARRATIVE_REMOTE="https://nextstrain.org/community/narratives/ktmeaton/plague-phylogeography/plagueSCDS2020Remote";
+# nextstrain_local2remote.sh User Repository Narrative Dataset
+# nextstrain_local2remote.sh ktmeaton plague-phylogeography plagueSCDS2020 plague150
 
-# nextstrain_local2remote.sh ktmeaton/paper-phylogeography
-#s|http://localhost:4000/plague150Local|https://nextstrain.org/community/ktmeaton/plague-phylogeography/plague150Remote|g
+USER=$1
+REPO=$2
+NARRATIVE=$3
+DATA=$4
 
-USER="ktmeaton"
-REPO="plague-phylogeography"
-NARRATIVE="plagueSCDS2020"
-DATA="plague150"
-
+# Defaults
 LOCAL_URL="http://localhost:4000"
 REMOTE_URL="https://nextstrain.org/community"
 NARRATIVE_DIR="narratives"
 
-echo ${LOCAL_URL}${NARRATIVE_DIR}${NARRATIVE}Local
 
 # First change data path to remote, then change narrative path to remote
 sed "s|${LOCAL_URL}/${DATA}Local|${REMOTE_URL}/${USER}/${REPO}/${DATA}Remote|g" ${NARRATIVE_DIR}/${NARRATIVE}Local.md | \
 	sed "s|${LOCAL_URL}/${NARRATIVE_DIR}/${NARRATIVE}Local|${REMOTE_URL}/${NARRATIVE_DIR}/${USER}/${REPO}/${NARRATIVE}Remote|g" \
 	> ${NARRATIVE_DIR}/${REPO}_${NARRATIVE}Remote.md
-
-
-# sed "s|${DATA_LOCAL}|${DATA_REMOTE}|g" ${MARKDOWN_LOCAL} | sed "s|${NARRATIVE_LOCAL}|${NARRATIVE_REMOTE}|g" 
-
-
