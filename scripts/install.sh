665690e062b5ff67e256925d7a8a92e48c874bc3
Katherine Eaton <ktmeaton@gmail.com>
2020-07-24 18:06:59 -0400

    update steps and exec mode

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,38 +1,53 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# Configure install location prefix
+# Default is /usr/local/bin (assumes sudo)
+prefix=$1
+if [[ -z $PREFIX ]]; then
+  prefix="/usr/local/bin";
+fi
+
+# Number of install steps
+STEPS="7"
+
+# Install conda (not tested)
+if [[ -z `which conda` ]]; then
+  echo "[Pre/${STEPS}] Installing conda."
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  chmod 755 Miniconda3-latest-Linux-x86_64.sh
+  ./Miniconda3-latest-Linux-x86_64.sh
+  conda config --set auto_activate_base False
+fi
+
+# Install nextflow (not tested)
+if [[ -z `which nextflow` ]]; then
+  echo "[Pre/${STEPS}] Installing nextflow."
+  wget -qO- get.nextflow.io | bash
+  mv nextflow /usr/local/bin/
+fi
 
 # Install the plague-phylogeography pipeline
-echo "Installing the plague-phylogeography nextflow pipeline."
-echo "nextflow pull ktmeaton/plague-phylogeography"
+echo "[1/${STEPS}] Installing the plague-phylogeography nextflow pipeline."
 nextflow pull ktmeaton/plague-phylogeography
 # Create the plague-phylogeography conda environment
-echo "Creating the plague-phylogeography conda environment."
-echo "conda env create -f  ~/.nextflow/assets/ktmeaton/plague-phylogeography/environment.yaml"
+echo "[2/${STEPS}] Creating the plague-phylogeography conda environment."
 conda env create -f  ~/.nextflow/assets/ktmeaton/plague-phylogeography/environment.yaml
 
 # Install the nfcore/eager pipeline
-echo "Installing the nf-core/eager nextflow pipeline."
-echo "nextflow pull nf-core/eager"
+echo "[3/${STEPS}] Installing the nf-core/eager nextflow pipeline."
 nextflow pull nf-core/eager
-echo "nextflow pull nf-core/eager -r 7b51863957"
 nextflow pull nf-core/eager -r 7b51863957
 # Create the nf-core/eager conda environment
-echo "Creating the nf-core/eager conda environment."
-echo "conda env create -f ~/.nextflow/assets/nf-core/eager/environment.yml"
+echo "[4/${STEPS}] Creating the nf-core/eager conda environment."
 conda env create -f ~/.nextflow/assets/nf-core/eager/environment.yml
-echo "Installing supplementary programs to the nf-core/eager environment."
-echo "conda install -n nf-core-eager-2.2.0dev -c bioconda nextflow==20.01.0"
+echo "[5/${STEPS}] Installing supplementary programs to the nf-core/eager environment."
 conda install -n nf-core-eager-2.2.0dev -c bioconda nextflow==20.01.0
-echo "conda install -n nf-core-eager-2.2.0dev -c anaconda graphviz"
 conda install -n nf-core-eager-2.2.0dev -c anaconda graphviz
 
 # Create the nextstrain conda environment
-echo "Creating the nextstrain conda environment."
-echo "conda env create -f  ~/.nextflow/assets/ktmeaton/plague-phylogeography/config/nextstrain.yaml"
+echo "[6/${STEPS}] Creating the nextstrain conda environment."
 conda env create -f  ~/.nextflow/assets/ktmeaton/plague-phylogeography/config/nextstrain.yaml
-echo "Installing supplementary programs to the nextstrain environment."
-echo "conda activate nextstrain-8.0.0"
+echo "[7/${STEPS}] Installing supplementary programs to the nextstrain environment."
 conda activate nextstrain-8.0.0
-echo "npm install --global auspice@2.17.0"
 npm install --global auspice@2.17.0
-echo "conda deactivate"
 conda deactivate
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,29 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# Cannot be run from within the nextflow directory
+# copy script elsewhere
 
 EAGER_CONDA_ENV="nf-core-eager-2.2.0dev"
 EAGER_NF_REV="7b51863957"
 PHYLO_CONDA_ENV="plague-phylogeography-0.1.4dev"
 NEXTSTRAIN_CONDA_ENV="nextstrain-8.0.0"
 
+# Number of uninstall steps
+STEPS="5"
+
 # Remove the plague-phylogeography pipeline
-echo "Removing the plague-phylogeography nextflow pipeline."
-echo "nextflow drop ktmeaton/plague-phylogeography"
+echo "[1/${STEPS}] Removing the plague-phylogeography nextflow pipeline."
 nextflow drop ktmeaton/plague-phylogeography
 # Remove the plague-phylogeography conda env
-echo "Removing the plague-phylogeography conda environment."
-echo "conda remove -n ${PHYLO_CONDA_ENV} --all"
+echo "[2/${STEPS}] Removing the plague-phylogeography conda environment."
 conda remove -n ${PHYLO_CONDA_ENV} --all
 
 # Remove the nfcore/eager pipeline
-echo "Removing the nf-core/eager nextflow pipeline."
-echo "nextflow drop nf-core/eager"
+echo "[3/${STEPS}] Removing the nf-core/eager nextflow pipeline."
 nextflow drop nf-core/eager
 # Remove the nfcore/eager conda env
-echo "Removing the nf-core/eager conda environment."
-echo "conda remove -n ${EAGER_CONDA_ENV} --all"
+echo "[4/${STEPS}] Removing the nf-core/eager conda environment."
 conda remove -n ${EAGER_CONDA_ENV} --all
 
 # Remove the nextstrain conda env
-echo "Removing the nextstrain conda environment."
+echo "[5/${STEPS}] Removing the nextstrain conda environment."
 echo "conda remove -n ${NEXTSTRAIN_CONDA_ENV} --all"
 conda remove -n ${NEXTSTRAIN_CONDA_ENV} --all
