0315666d499729894098397a830b9a2ba902455c
Katherine Eaton <ktmeaton@gmail.com>
2020-07-25 09:21:18 -0400

    conda env checking before uninstall

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -9,42 +9,43 @@ REPO=${1:-"ktmeaton/plague-phylogeography"}
 
 # Number of uninstall steps
 STEPS="5"
-# Get plague-phylogeography conda name
-PHYLO_CONDA_ENV="plague-phylogeography-0.1.4dev"
-if [[ -f ~/.nextflow/assets/${REPO}/environment.yaml ]]; then
-  PHYLO_CONDA_ENV=`grep "name:" ~/.nextflow/assets/${REPO}/environment.yaml |  \
-                   cut -d " " -f 2`
-fi
 
-
+#------------------------------------------------------------------------------#
 # Remove the plague-phylogeography pipeline and conda environment
+PHYLO_CONDA_ENV="plague-phylogeography-0.1.4dev"
 echo "[1/${STEPS}] Removing the plague-phylogeography nextflow pipeline."
 if [[ -f ~/.nextflow/assets/${REPO}/environment.yaml ]]; then
   PHYLO_CONDA_ENV=`grep "name:" ~/.nextflow/assets/${REPO}/environment.yaml |  \
                    cut -d " " -f 2`
   nextflow drop ${REPO}
-  echo "[2/${STEPS}] Removing the plague-phylogeography conda environment."
+fi
+echo "[2/${STEPS}] Removing the plague-phylogeography conda environment."
+if [[ `conda env list | grep ${PHYLO_CONDA_ENV}` ]]; then
   conda remove -n ${PHYLO_CONDA_ENV} --all
-else
-  echo "[2/${STEPS}] Removing the plague-phylogeography conda environment...Skip."
 fi
 
-# Remove the nfcore/eager pipeline
+#------------------------------------------------------------------------------#
+# Remove the nfcore/eager pipeline and conda env
+EAGER_CONDA_ENV="nf-core-eager-2.2.0dev"
 echo "[3/${STEPS}] Removing the nf-core/eager nextflow pipeline."
 if [[ -f ~/.nextflow/assets/nf-core/eager/environment.yaml ]]; then
-  EAGER_CONDA_ENV=`grep "name:" ~/.nextflow/assets/nf-core/eager/environment.yaml |  \
+  EAGER_CONDA_ENV=`grep "name:" ~/.nextflow/assets/nf-core/eager/environment.yaml | \
                    cut -d " " -f 2`
   nextflow drop nf-core/eager
-  echo "[4/${STEPS}] Removing the nf-core/eager conda environment."
+fi
+echo "[4/${STEPS}] Removing the nf-core/eager conda environment."
+if [[ `conda env list | grep ${EAGER_CONDA_ENV}` ]]; then
   conda remove -n ${EAGER_CONDA_ENV} --all
-else
-  echo "[4/${STEPS}] Removing the nf-core/eager conda environment...Skip."
 fi
 
+#------------------------------------------------------------------------------#
 # Remove the nextstrain conda env
+NEXTSTRAIN_CONDA_ENV="nextstrain-8.0.0"
 echo "[5/${STEPS}] Removing the nextstrain conda environment."
 if [[ -f ~/.nextflow/assets/${REPO}/config/nextstrain.yaml ]]; then
   NEXTSTRAIN_CONDA_ENV=`grep "name:" ~/.nextflow/assets/${REPO}/config/nextstrain.yaml |  \
                    cut -d " " -f 2`
+fi
+if [[ `conda env list | grep ${NEXTSTRAIN_CONDA_ENV}` ]]; then
   conda remove -n ${NEXTSTRAIN_CONDA_ENV} --all
 fi
