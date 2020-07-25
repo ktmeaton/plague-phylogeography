71bed77d7f28f21446c881e7a33ae1e00fc67c0a
Katherine Eaton <ktmeaton@gmail.com>
2020-07-25 14:30:08 -0400

    check conda env before trying to create

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ REPO=${1:-"ktmeaton/plague-phylogeography"}
 SHA=${2:-"master"}
 
 # Gloabl script variables
-STEPS="7"
+STEPS="5"
 NF_VER="20.01.0"
 EAGER_NF_REV="7b51863957"
 AUSPICE_VER="2.17.0"
@@ -18,10 +18,15 @@ AUSPICE_VER="2.17.0"
 echo "[1/${STEPS}] Installing the ${REPO} nextflow pipeline - ${SHA}."
 nextflow pull ${REPO}
 nextflow pull ${REPO} -r ${SHA}
+
 # Create the plague-phylogeography conda environment
 echo "[2/${STEPS}] Creating the plague-phylogeography conda environment."
-conda env create -f  ~/.nextflow/assets/${REPO}/environment.yaml
-
+# Check if environment already exists
+PHYLO_CONDA_ENV=`grep "name:" ~/.nextflow/assets/${REPO}/environment.yaml |  \
+                 cut -d " " -f 2`
+if [[ ! `conda env list | grep ${PHYLO_CONDA_ENV}` ]]; then
+ conda env create -f  ~/.nextflow/assets/${REPO}/environment.yaml
+fi
 
 # Install the nfcore/eager pipeline
 echo "[3/${STEPS}] Installing the nf-core/eager nextflow pipeline."
@@ -32,18 +37,22 @@ nextflow pull nf-core/eager -r ${EAGER_NF_REV}
 echo "[4/${STEPS}] Creating the nf-core/eager conda environment."
 EAGER_CONDA_ENV=`grep "name:" ~/.nextflow/assets/nf-core/eager/environment.yml | \
                  cut -d " " -f 2`
-conda env create -f ~/.nextflow/assets/nf-core/eager/environment.yml
-echo "[5/${STEPS}] Installing supplementary programs to the nf-core/eager environment."
-conda install -n ${EAGER_CONDA_ENV} -c bioconda nextflow==${NF_VER}
-conda install -n ${EAGER_CONDA_ENV} -c anaconda graphviz
-
+# Check if environment already exists
+if [[ ! `conda env list | grep ${EAGER_CONDA_ENV}` ]]; then
+  conda env create -f ~/.nextflow/assets/nf-core/eager/environment.yml
+  conda install -n ${EAGER_CONDA_ENV} -c bioconda nextflow==${NF_VER}
+  conda install -n ${EAGER_CONDA_ENV} -c anaconda graphviz
+fi
 
 # Create the nextstrain conda environment
-echo "[6/${STEPS}] Creating the nextstrain conda environment."
-conda env create -f  ~/.nextflow/assets/${REPO}/config/nextstrain.yaml
+echo "[5/${STEPS}] Creating the nextstrain conda environment."
 NEXTSTRAIN_CONDA_ENV=`grep "name:" ~/.nextflow/assets/ktmeaton/plague-phylogeography/config/nextstrain.yaml | \
                       cut -d " " -f 2`
-echo "[7/${STEPS}] Installing supplementary programs to the nextstrain environment."
-conda activate ${NEXTSTRAIN_CONDA_ENV}
-npm install --global auspice@${AUSPICE_VER}
-conda deactivate
+# Check if environment already exists
+if [[ ! `conda env list | grep ${NEXTSTRAIN_CONDA_ENV}` ]]; then
+  conda env create -f  ~/.nextflow/assets/${REPO}/config/nextstrain.yaml
+  source ~/miniconda3/etc/profile.d/conda.sh
+  conda activate ${NEXTSTRAIN_CONDA_ENV}
+  npm install --global auspice@${AUSPICE_VER}
+  conda deactivate
+fi
