88369b357a12061739006cc5b1e4b401bb75921f
Katherine Eaton <ktmeaton@gmail.com>
2020-09-30 16:26:26 -0400

    remove extra python script, add start jobs log

--- workflow/scripts/slack_log.py
+++ workflow/scripts/slack_log.py
@@ -3,7 +3,6 @@ from slack import WebClient
 import platform  # Get system name
 from dotenv import load_dotenv  # Reading env files
 from datetime import datetime  # Logging time
-import argparse  # Parse arguments if this script is main
 
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
@@ -26,76 +25,37 @@ node_name = platform.node()
 now = datetime.now()
 current_time = now.strftime("%H:%M")
 
-# If this script was not imported by snakemake, perform a custom slack operation
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(add_help=True,)
-    parser.add_argument(
-        "--rules",
-        help="Rule(s) successfully completed by snakemake in csv format.",
-        action="store",
-        dest="rulesList",
-        required=True,
-    )
-    parser.add_argument(
-        "--complete",
-        help="Include flag is snakemake rules are complete.",
-        action="store_true",
-        dest="rulesComplete",
-        required=False,
-    )
-
-    args = vars(parser.parse_args())
-    rules_list = args["rulesList"].split(",")
-    rules_complete = args["rulesComplete"]
-
-    # If we're only beginning
-    if not rules_complete:
-        client.chat_postMessage(
-            channel="snakemake",
-            blocks=[
-                {"type": "divider"},
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": ":heavy_check_mark: Snakemake workflow on *"
-                        + node_name
-                        + "* started the following rules at *"
-                        + current_time
-                        + "*.\n"
-                        + "\n".join(["\t\t - " + rule for rule in rules_list]),
-                    },
-                },
-            ],
-        )
-    if rules_complete:
-        client.chat_postMessage(
-            channel="snakemake",
-            blocks=[
-                {"type": "divider"},
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": ":heavy_check_mark: Snakemake workflow on *"
-                        + node_name
-                        + "* completed the following rules at "
-                        + "*"
-                        + current_time
-                        + "*.\n"
-                        + "\n".join(["\t\t - " + rule for rule in rules_list]),
-                    },
-                },
-            ],
-        )
-
 
 # If this script was imported by snakemake, use the default log_handler function
 if __name__ != "__main__":
 
     def log_handler(msg):
         """A log handler for slack integration., adapted from snakemake"""
-        print(msg)
+        # print(msg)
+        # --------------------------------------------------------------------------#
+        # Job Start
+        # --------------------------------------------------------------------------#
+        if msg["level"] == "run_info":
+            # print(msg['msg'])
+            client.chat_postMessage(
+                channel="snakemake",
+                blocks=[
+                    {"type": "divider"},
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": ":cyclone: Snakemake workflow on *"
+                            + node_name
+                            + "* started running the following rules at "
+                            + "*"
+                            + current_time
+                            + "*.",
+                        },
+                    },
+                    {"type": "section", "text": {"type": "mrkdwn", "text": msg["msg"]}},
+                ],
+            )
         # --------------------------------------------------------------------------#
         # Error message
         # --------------------------------------------------------------------------#
