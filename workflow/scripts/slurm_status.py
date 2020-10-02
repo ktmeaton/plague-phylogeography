b4b2fe265d6fb2e95664f8d9ceaf5cc6aa97248c
Katherine Eaton <ktmeaton@gmail.com>
2020-10-02 14:43:16 -0400

    use a slurm-specific status check script

--- workflow/scripts/slurm_status.py
+++ workflow/scripts/slurm_status.py
@@ -1,17 +1,69 @@
-#!/usr/bin/env python
-import subprocess
+#!/usr/bin/env python3
+import re
+import subprocess as sp
+import shlex
 import sys
+import time
+import logging
 
-args = sys.argv[:]
-jobid_ind = args.index("job") + 1
-jobid = args[jobid_ind]
+logger = logging.getLogger("__name__")
 
-output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
+STATUS_ATTEMPTS = 20
 
-running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED"]
-if "COMPLETED" in output:
-  print("success")
-elif any(r in output for r in running_status):
-  print("running")
+jobid = sys.argv[1]
+
+for i in range(STATUS_ATTEMPTS):
+    try:
+        sacct_res = sp.check_output(shlex.split("sacct -P -b -j {} -n".format(jobid)))
+        res = {
+            x.split("|")[0]: x.split("|")[1]
+            for x in sacct_res.decode().strip().split("\n")
+        }
+        break
+    except sp.CalledProcessError as e:
+        logger.error("sacct process error")
+        logger.error(e)
+    except IndexError:
+        pass
+    # Try getting job with scontrol instead in case sacct is misconfigured
+    try:
+        sctrl_res = sp.check_output(
+            shlex.split("scontrol -o show job {}".format(jobid))
+        )
+        m = re.search(r"JobState=(\w+)", sctrl_res.decode())
+        res = {jobid: m.group(1)}
+        break
+    except sp.CalledProcessError as e:
+        logger.error("scontrol process error")
+        logger.error(e)
+        if i >= STATUS_ATTEMPTS - 1:
+            print("failed")
+            exit(0)
+        else:
+            time.sleep(1)
+
+status = res[jobid]
+
+if status == "BOOT_FAIL":
+    print("failed")
+elif status == "OUT_OF_MEMORY":
+    print("failed")
+elif status.startswith("CANCELLED"):
+    print("failed")
+elif status == "COMPLETED":
+    print("success")
+elif status == "DEADLINE":
+    print("failed")
+elif status == "FAILED":
+    print("failed")
+elif status == "NODE_FAIL":
+    print("failed")
+elif status == "PREEMPTED":
+    print("failed")
+elif status == "TIMEOUT":
+    print("failed")
+# Unclear whether SUSPENDED should be treated as running or failed
+elif status == "SUSPENDED":
+    print("failed")
 else:
-  print("failed")
+    print("running")
