df071bf91d90a9561fb7dfe8904b0ea4b72b7246
Katherine Eaton <ktmeaton@gmail.com>
2021-02-27 15:11:25 -0500

    add custom genes to locus coverage

--- docs/notes/Notes_Development.md
+++ docs/notes/Notes_Development.md
@@ -4,8 +4,12 @@
 1. Add a heatmap test, output to misc.
 1. Create a new rule to generate locus coverage.
 1. Remove output directory of eager rule.
+1. Make a list of target genes
+    - pPCP1 - pla (AL109969,6665,7603)
 
 ## TO-DO
 
 1. Create gene by gene coverage.
 1. Add locus coverage output to report
+1. Check if multiqc still runs without eager output dir...
+1. Move ref locus bed to reference
--- workflow/scripts/locus_coverage.sh
+++ workflow/scripts/locus_coverage.sh
@@ -13,9 +13,15 @@ for i in $(seq 1 ${#name[@]})
 do
     echo -e "${name[$i -1]}\t0\t${end[$i -1]}" >> $REF_BED;
 done
-
-# Print the output header
+# Configure the auto header
 header=`echo -e "Sample\t${name[@]}" | sed 's/ /\t/g'`
+
+# Add manual loci to bed
+# plasminogen activation (pla)
+echo -e "AL109969\t6665\t7603" >> $REF_BED
+# Add manual loci to header
+header=$header"\tpla"
+# Print the output header
 echo -e "$header"
 
 # Analyze the bamfiles
@@ -25,4 +31,5 @@ do
     sample=`basename $bam | sed 's/\.bam//g'`;
     cov=`samtools view -b -q 30 $bam | bedtools coverage -a ${REF_BED} -b stdin | cut -f 7 | tr '\n' '\t'`;
     echo -e "$sample\t$cov";
+    exit
 done
