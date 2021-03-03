ff68c7e789a587b44ed22b149da473ed29828af5
Katherine Eaton <ktmeaton@gmail.com>
2021-03-03 13:24:17 -0500

    create new script to backup results

---
+++ workflow/scripts/backup_results.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RESULTS_DIR=$1
+BACKUP_DIR=$2
+CONFIRM=$3
+
+EXCLUDE_DIR=(
+	data
+	sqlite_db
+	eager
+	snippy_pairwise
+	qualimap
+	);
+
+if [[ ! $RESULTS_DIR ]];
+then
+	exit 1;
+fi
+
+echo -e "\nBacking up ${RESULTS_DIR} to ${BACKUP_DIR}:"
+mkdir -p $BACKUP_DIR;
+
+for dir in `ls -d $RESULTS_DIR/*`;
+do
+	dirname=`basename $dir`;
+	# Iterate through target directories to exclude
+	keep="true";
+	for target in ${EXCLUDE_DIR[@]};
+	do
+		if [[ $dirname == $target ]];
+		then
+			keep="false";
+	  fi;
+	done;
+	# Check if this directory should be kept
+	if [[ $keep == "true" && $CONFIRM ]];
+	then
+		echo -e "\tBacking up: ${RESULTS_DIR}/$dirname";
+		echo -e "\t            cp -r ${RESULTS_DIR}/$dirname to ${BACKUP_DIR}/$dirname";
+		cp -r ${RESULTS_DIR}/$dirname ${BACKUP_DIR}/$dirname
+	fi
+done;
