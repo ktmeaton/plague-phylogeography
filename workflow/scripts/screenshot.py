fd67797156ba090fb5c5a44ac2a14b4919dd7b80
Katherine Eaton <ktmeaton@gmail.com>
2021-08-03 22:49:15 -0400

    add first pandemic to multi plot

--- workflow/scripts/screenshot.py
+++ workflow/scripts/screenshot.py
@@ -4,35 +4,64 @@ import asyncio
 from pyppeteer import launch
 import time
 
-# dataset = (
-#  "https://nextstrain.org/community/" +
-#  "ktmeaton/plague-phylogeography-projects@main/main/full/all"
-# )
-# query = "?d=tree&legend=open&m=div&onlyPanels&p=full&sidebar=closed"
+# ------------------------------------------
+# URL Construction
 
-# LOCAL
-dataset = "http://localhost:4000/1.PRE"
-query = "?c=date_mean&d=map&m=div&onlyPanels&p=full&sidebar=closed&tl=blank"
-url = dataset + query
+clade = "1.ORI"
+dataset = "main/beast/geo/" + clade
+geo_res = "province"
+color = "continent"
+panel = "map"
+transmissions = "show"
+repo = "plague-phylogeography-projects"
+base_url = "https://nextstrain.org/community/ktmeaton"
+branch = "main"
+
+project = base_url + "/" + repo + "@" + branch + "/" + dataset
+options = [
+    "r=" + geo_res,
+    "c=" + color,
+    "d=" + panel,
+    "sidebar=closed",
+    "transmissions=" + transmissions,
+    "legend=closed",
+    "p=full",
+    "onlyPanels",
+]
+query = "?" + "&".join(options)
+url = project + query
+
+output = "{}_{}_{}.pdf".format(clade, panel, color)
 
 print(url)
 print()
 
 wait = 10
-width = 450
-height = 450
+width = 720
+height = 720
+options = {
+    "path": output,
+    "format": "A0",
+    "printBackground": True,
+    "landscape": True,
+}
 
 
 async def main():
     browser = await launch()
     page = await browser.newPage()
+
     await page.setViewport({"width": width, "height": height, "deviceScaleFactor": 4})
     print("Waiting {} seconds for Nextstrain to load page...".format(wait))
+
+    # Navigate to page
     await page.goto(url)
+    # Change to landscape
     time.sleep(wait)
     print("Taking a pdf screenshot...")
     await page.emulateMedia("screen")
-    await page.pdf({"path": "screenshot.pdf"})
+    await page.pdf(options)
+    print("Saved screenshot to " + output)
     await browser.close()
 
 
